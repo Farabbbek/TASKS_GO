a07e87f11422083508a5bb0abee0698014cc609c
atusup <ftusup@bk.ru>
2025-09-16 16:29:16 +0300

    second

---
+++ third/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"sync"
+)
+
+type MapFunc[T any, R any] func(T) R
+
+func ParallelMap[T any, R any](in []T, fn MapFunc[T, R], workers int) []R {
+	out := make([]R, len(in))
+	var wait sync.WaitGroup
+	tasks := make(chan struct {
+		a int
+		b T
+	}, len(in))
+	for w := 0; w < workers; w++ {
+		wait.Add(1)
+		go func() {
+			defer wait.Done()
+			for task := range tasks {
+				out[task.a] = fn(task.b)
+			}
+		}()
+	}
+
+	for i, v := range in {
+		tasks <- struct {
+			a int
+			b T
+		}{i, v}
+	}
+
+	close(tasks)
+	wait.Wait()
+	return out
+}
