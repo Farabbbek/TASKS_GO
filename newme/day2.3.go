1ce0c2cb432d2606a4255ab2c932837f6f4d481e
Al-Farabi <farabi@Al-Farabi.local>
2026-07-21 20:38:17 +0500

    slice,array

---
+++ newme/day2.3.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//array
+	arr := [5]int{73, 98, 86, 61, 96}
+
+	fmt.Println(arr)
+	fmt.Println(arr[3])
+
+	arr[4] = 100
+	fmt.Println(arr)
+
+	//slice
+	s := []int{73, 98, 86, 61, 96}
+	fmt.Println("Длина =", len(s), "вместимость =", cap(s))
+	fmt.Println(s)
+	s[2] = 33
+	s = append(s, 999)
+	fmt.Println(s)
+	fmt.Println("Длина =", len(s), "вместимость =", cap(s))
+
+	//slice с фиксированной вместимостью
+	s2 := make([]int, 5, 500)
+	fmt.Println(s2)
+	fmt.Println("Длина =", len(s2), "вместимость =", cap(s2))
+
+	//срезы
+	//s := []int{73, 98, 86, 61, 96}
+	s3 := s[0:2:2]
+	fmt.Println(s3)
+	fmt.Println("Длина =", len(s3), "вместимость =", cap(s3))
+
+}
